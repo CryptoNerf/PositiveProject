7aa190d692ff3bef0d84247161c0c0f75a2042a6
Emile Alexanyan <nerf.file@gmail.com>
2026-05-02 15:32:57 +0400

    Chat_Add

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'positive-v2';
+const PRECACHE = ['./', './index.html', './image/клеткафон.jpg', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(PRECACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = e.request.url;
+  // Don't intercept Firebase / Google requests
+  if (url.includes('firebase') || url.includes('googleapis') || url.includes('gstatic')) return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
